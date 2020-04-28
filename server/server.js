6dc24e241cb107ac2d478f4dc829bcee338d852e
z0gSh1u <zx.cs@qq.com>
2020-04-28 23:27:10 +0800

    Push forward.

---
+++ server/server.js
@@ -0,0 +1,48 @@
+/**
+ * Node.js backend for chet-rum.
+ * by z0gSh1u @ 2020-04
+ */
+
+const path = require('path')
+const express = require('express')
+const app = express()
+app.use(express.static(path.join(__dirname, '../view')))
+const http = require('http').createServer(app)
+const sio = require('socket.io')(http)
+
+let userSockets = [] // { username, socket }[]
+sio.on('connection', socket => {
+
+  /**
+   * Login process.
+   */
+  socket.on('newJoin', ({ username }) => {
+    let found = false
+    for (let i = 0; i < userSockets.length; i++) {
+      if (userSockets[i].username == username) {
+        userSockets[i].socket = socket
+        found = true
+        console.log('[newJoin] Old user: ' + username)
+        break
+      }
+    }
+    if (!found) {
+      userSockets.push({ username, socket })
+      console.log('[newJoin] New user: ' + username)
+    }
+  })
+
+  /**
+   * A new message from client is sent. Broadcast it.
+   */
+  socket.on('newMsg', ({ username, content }) => {
+    sio.emit('broadcastMsg', { username, content })
+  })
+})
+
+/**
+ * Server start.
+ */
+http.listen(3000, () => {
+  console.log('Listening Port 3000.')
+})
